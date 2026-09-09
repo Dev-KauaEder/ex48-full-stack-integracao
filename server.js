e1db872d86ce973fda74010db5232bfeb50f5980
Dev-KauaEder <programador.kaua19@gmail.com>
2026-09-09 11:48:46 -0300

    Tarefa concluida , implementei a API

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ app.use(express.static('public'));
 
 let tarefas = [], nextId = 1;
 
+app.get('/api/tarefas',( req , res )=> {
+    res.status(200).json();
+})
+
+app.post('/api/tarefas', (req, res) => {
+    const { titulo } = req.body;
+
+    const tarefa = {
+        id: nextId++,
+        titulo: titulo
+    };
+
+    tarefas.push(tarefa)
+
+    res.status(201).json(tarefa)
+})
 // TODO:
 // GET  /api/tarefas  → 200 + array de tarefas
 // POST /api/tarefas  → 201 + tarefa criada { id, titulo }
